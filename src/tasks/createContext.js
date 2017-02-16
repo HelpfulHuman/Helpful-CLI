fb9e03dd01eee4769f68361578fe80efbb99f331
Nick Glenn <nick@helpfulhuman.com>
2017-02-15 23:22:59 -0800

    Working on defining the context schema

--- src/tasks/createContext.js
+++ src/tasks/createContext.js
@@ -3,6 +3,10 @@ import path from "path";
 /**
  * Sets up the initial structure of the context object using only
  * a sourcePath and targetPath string.
+ *
+ * @param  {String} sourcePath
+ * @param  {String} targetPath
+ * @return {Object}
  */
 export default function createContext (sourcePath, targetPath) {
   return {
@@ -15,6 +19,11 @@ export default function createContext (sourcePath, targetPath) {
       originalPath: targetPath,
       path: path.resolve(targetPath)
     },
-    input: {}
+    input: {},
+    manifest: {
+      ask: [],
+      copy: {},
+      run: []
+    }
   };
 }
