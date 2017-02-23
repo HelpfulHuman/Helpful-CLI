8086e7be9de61752777fb0d782d5086a3b3b8e71
Nick Glenn <nick@helpfulhuman.com>
2017-02-22 21:19:33 -0800

    Smal updates to the ending notifications task

--- src/tasks/endingNotification.js
+++ src/tasks/endingNotification.js
@@ -18,10 +18,10 @@ export default function (err, ctx) {
  */
 function printError (err) {
   process.stdout.write(`\n\n
-  ${chalk.bold.bgRed("Failure!  Error!  Sadness!")}\n
-  Oh man, this is embarrassing but we weren't able to complete the operation.\n\n
-  ${chalk.bold("Why?")}\n
-  ${chalk.red(err.message)}
+${chalk.bold.bgRed("Failure!  Error!  Sadness!")}\n
+Oh man, this is embarrassing but we weren't able to complete the operation.\n\n
+${chalk.bold("Why?")}\n
+${chalk.red(err.message)}
   `);
   process.exit(1);
 }
@@ -31,7 +31,7 @@ function printError (err) {
  */
 function printSuccess (ctx) {
   process.stdout.write(`\n\n
-  ${chalk.bold.green(figures.tick + " Success!")}\n
-  We successfully added the package's contents to your current directory.\n\n
+${chalk.bold.green(figures.tick + " Success!")}\n
+We successfully added the package's contents to your current directory.\n\n
   `);
 }
