5f63b5f344edb0495a8adaca59577386914cd6f3
Nick Glenn <nick@helpfulhuman.com>
2017-02-22 23:44:12 -0800

    Cleaning up deprecated files, see v1 branch for references

--- commands/init/template/react/client/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-import authReducer from './auth';
-
-export default combineReducers({
-  routing: routerReducer,
-  auth: authReducer
-});
