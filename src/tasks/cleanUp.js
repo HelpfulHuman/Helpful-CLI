051924a5ede90172af97dd0245e784b988b6d296
Nick Glenn <nick@helpfulhuman.com>
2017-02-22 21:20:09 -0800

    Added a clean up task

---
+++ src/tasks/cleanUp.js
@@ -0,0 +1,17 @@
+import fs from "fs-extra";
+import * as status from "../utils/status";
+
+/**
+ * Manually remove the temp files, just to be safe.
+ *
+ * @param  {Object} ctx
+ * @param  {Callback<Error>} next
+ */
+export default function (ctx, next) {
+  status.report("Cleaning up temporary files");
+  fs.remove(ctx.paths.temp, function (err) {
+    if (err) status.fail();
+    else status.complete();
+    next(err);
+  });
+}
